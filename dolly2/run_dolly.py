4a60d5fe609f0749ed3a9a8884fe767e8f507d18
z <z@sp.sp>
2023-08-15 15:40:07 +0300

    dolly2

---
+++ dolly2/run_dolly.py
@@ -0,0 +1,24 @@
+# pip install transformers
+# pip install accelerate
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+from instruct_pipeline import InstructionTextGenerationPipeline
+
+base_model = "databricks/dolly-v2-12b"
+load_8bit = True
+
+tokenizer = AutoTokenizer.from_pretrained(base_model, padding_side="left")
+model = AutoModelForCausalLM.from_pretrained(
+    base_model,
+    load_in_8bit=load_8bit,
+    torch_dtype=torch.float16,
+    device_map="auto"
+)
+
+model.eval()
+if torch.__version__ >= "2":
+    model = torch.compile(model)
+
+pipe = InstructionTextGenerationPipeline(model=model, tokenizer=tokenizer)
+pipe("Write an example of JSON file:")
